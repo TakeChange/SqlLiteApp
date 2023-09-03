5762e66a679fb0d401e1e3d59405fc0c373f6d4e
PritishWa <92771064+PritishWa@users.noreply.github.com>
2023-09-03 19:52:12 +0530

    spe03 push data

--- src/screen/FirstScreen.js
+++ src/screen/FirstScreen.js
@@ -1,67 +1,98 @@
-import { StyleSheet, Text, View,FlatList,TouchableOpacity } from 'react-native'
-import React, {useState }  from 'react'
+import { StyleSheet, Text, View, FlatList, TouchableOpacity,ToastAndroid } from 'react-native'
+import React, { useState,useEffect } from 'react'
 import Ionicons from 'react-native-vector-icons/Ionicons'
+import { openDatabase } from 'react-native-sqlite-storage';
+import { useIsFocused } from '@react-navigation/native';
 
 const FirstScreen1 = ({ navigation }) => {
-  const [Emp, setEmp] = useState([]);
-    const Employee = [
-        {
-          Emp_Name:'Mr.Sharma',
-          Emp_Age:30,
-          Emp_Address:'Pune',
-        },
-        {
-          Emp_Name:'Mr.Gupta',
-          Emp_Age:35,
-          Emp_Address:'Nashik', 
-        },
-    ];    const EmpInfo = ({ item }) => {
-      return (
-        <View style={styles.Container}>
-          <View>
-            <Text style={{ fontSize: 15,fontWeight:'900',color: '#1D1E20'}}>Emp Name: {item.Emp_Name}</Text>
-            <Text style={{ fontSize: 15, fontWeight: '900',color: '#1D1E20'}}>Emp Age: {item.Emp_Age}</Text>
-            <Text style={{ fontSize: 15, fontWeight: '900',color: '#1D1E20'}}>Emp Address: {item.Emp_Address}</Text>
-            
 
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center',marginTop:'6%',alignItems:'center' }}>
-                <TouchableOpacity onPress={()=>navigation.navigate('UpdateEmp')}>
-                    <Text style={{fontWeight:'900',color:'white',backgroundColor:'gray',padding:'5%',borderRadius:10}}> EDIT</Text>
-                </TouchableOpacity>
-                <TouchableOpacity>
-                    <Text style={{fontWeight:'900',color:'white',backgroundColor:'gray',padding:'5%',borderRadius:10}}> DELETE</Text>
-                </TouchableOpacity>
-                </View>
-            </View> 
+  const db = openDatabase({ name: 'EmployeeDatabase.db' });
+  const [Emp, setEmp] = useState([]);
+  const [empList,setEmpList] = useState([])
+  const isFocused = useIsFocused();
+  useEffect(()=>{
+    getData();
+  },[isFocused])
+
+  const getData = () => {
+    db.transaction(tx => {
+      tx.executeSql('SELECT * FROM employee', [], (tx, results) => {
+        var temp = [];
+        for (let i = 0; i < results.rows.length; ++i)
+        {
+          temp.push(results.rows.item(i));
+          console.log(results.rows.item(i))
+        }
+        setEmpList(temp);
+      });
+    });
+  };
+
+  const deleteEmp = (id) =>{
+    db.transaction(tx => {
+      tx.executeSql(
+        'DELETE FROM employee where user_id=?',
+        [id],
+        (tx, results) => {
+          console.log('Results', results.rowsAffected);
+          if (results.rowsAffected > 0) {
+            ToastAndroid.show('record deleted successfully', ToastAndroid.SHORT);
+            getData();
+          }
+        },
+      );
+    });
+  }
+
+  const EmpInfo = ({ item }) => {
+    return (
+      <View style={styles.Container}>
+        <View>
+          <Text style={{ fontSize: 15, fontWeight: '900', color: '#1D1E20' }}>Emp Name: {item.employee_name}</Text>
+          <Text style={{ fontSize: 15, fontWeight: '900', color: '#1D1E20' }}>Emp Age: {item.employee_age}</Text>
+          <Text style={{ fontSize: 15, fontWeight: '900', color: '#1D1E20' }}>Emp Address: {item.employee_address}</Text>
+
+
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginTop: '6%', alignItems: 'center' }}>
+            <TouchableOpacity onPress={() => navigation.navigate('UpdateEmp',{data:item})}>
+              <Text style={{ fontWeight: '900', color: 'white', backgroundColor: 'gray', padding: '5%', borderRadius: 10 }}> EDIT</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={()=>deleteEmp(item.user_id)}>
+              <Text style={{ fontWeight: '900', color: 'white', backgroundColor: 'gray', padding: '5%', borderRadius: 10 }}> DELETE</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-         
-      )
-    }
+      </View>
+
+    )
+  }
   return (
-    <View>
-    <FlatList
-            data={Employee}
-            renderItem={EmpInfo}
-            keyExtractor={(item) => item.Employee}
-          />
-          <View style={{marginTop:'45%',marginLeft:'75%'}}>
-                  <TouchableOpacity onPress={()=>navigation.navigate('SecondScreen')}>
-                  <Ionicons name='add-circle' color='#1D1E20' size={60} />
-                  </TouchableOpacity>
-              </View>
+    <View style={{flex:1}}>
+      <View>
+      <FlatList
+        data={empList}
+        renderItem={EmpInfo}
+        keyExtractor={(item) => item.Employee}
+      />
+      </View>
+      <View style={{ position:'absolute',bottom:10,alignSelf:'flex-end',right:10 }}>
+        <TouchableOpacity onPress={() => navigation.navigate('SecondScreen')}>
+          <Ionicons name='add-circle' color='#1D1E20' size={60} />
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
 export default FirstScreen1
 
 const styles = StyleSheet.create({
-  Container:{
-    marginTop:'10%',
+  Container: {
+    marginTop: '10%',
     backgroundColor: 'pink',
-    borderRadius:20,
-    padding:'8%',
+    borderRadius: 20,
+    padding: '8%',
     justifyContent: 'center',
-   margin:'6%'
+    margin: '6%'
   },
- 
+
 })
